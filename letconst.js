1b94a6e9de3abb066a7987a7f821a42c35bd1f68
saiful2612 <bristykodom@gmail.com>
2021-01-31 09:37:39 +0600

    try something new

---
+++ letconst.js
@@ -0,0 +1,16 @@
+// riyaz()
+
+// function riyaz(){
+//     var x=5;
+//     for (let i = 0; i <= x ; i++) {
+//         console.log('Riyaz');
+        
+//     }
+// }
+
+console.log(this)
+
+
+
+
+
