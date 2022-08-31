3b71ca699c414c9c57d632eff775a48cc0034048
saiful2612 <bristykodom@gmail.com>
2022-08-31 14:42:58 +0600

    add button handler function

--- button.js
+++ button.js
@@ -4,20 +4,34 @@ const subtractButton = document.getElementById("subtraction");
 
 
 
-// Button handlers
-addButton.addEventListener('click', () => {
+// button handler function
+
+function clicked(press) {
     const inputValue = document.getElementById("quantity");
     const number = parseInt(inputValue.value);
-    const value = parseInt(screen.innerText) + number;
-    screen.innerText = value;
-})
+    if (press === 'add') {
+        const value = parseInt(screen.innerText) + number;
+        screen.innerText = value;
+    } else {
+        const value = parseInt(screen.innerText) - number;
+        screen.innerText = value;
+    }
+
+    // screen.innerText = value;
+}
+
+// Button handlers
+addButton.addEventListener('click', () => {
+    clicked('add');
+});
 
 
 subtractButton.addEventListener('click', () => {
-    const inputValue = document.getElementById("quantity");
-    const number = parseInt(inputValue.value);
-    const value = parseInt(screen.innerText) - number;
-    screen.innerText = value;
+    clicked();
+    // const inputValue = document.getElementById("quantity");
+    // const number = parseInt(inputValue.value);
+    // const value = parseInt(screen.innerText) - number;
+    // screen.innerText = value;
 })
 
 
